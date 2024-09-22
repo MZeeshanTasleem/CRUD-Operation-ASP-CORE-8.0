52aceebfea3946249b998a88acbd372dbe8510fb
Zeeshan Tasleem <mzeeshantasleem@gmail.com>
2024-09-22 23:09:36 +0500

    Add project files.

---
+++ WebApplicationLogin/Controllers/StudentsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationLogin.Models;
+using WebApplicationLogin.Data;
+using WebApplicationLogin.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationLogin.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly AppData dbContext;
+        public StudentsController(AppData dbContext) 
+        {
+            this.dbContext=dbContext;
+        } 
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+            Email=viewModel.Email,
+            Phone=viewModel.Phone,
+            Subscribed=viewModel.Subscribed
+            };
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> list()
+        {
+            var students= await dbContext.Students.ToListAsync();
+            return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var students = await dbContext.Students.FindAsync(id);
+            return View(students);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student ViewModel)
+        {
+            var student = await dbContext.Students.FindAsync(ViewModel.Id);  
+            if(student is not null)
+            {
+                student.Name=ViewModel.Name;
+                student.Email=ViewModel.Email;  
+                student.Phone=ViewModel.Phone;  
+                student.Subscribed=ViewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("list", "Students");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student ViewModel)
+        {
+            var student = await dbContext.Students.AsNoTracking().FirstOrDefaultAsync(x => x.Id == ViewModel.Id);
+            if (student is not null)
+            {
+                dbContext.Students.Remove(ViewModel);
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("list", "Students");
+        }
+    }
+}
---
+++ WebApplicationLogin/Data/AppData.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationLogin.Models.Entities;
+
+namespace WebApplicationLogin.Data
+{
+    public class AppData : DbContext
+    {
+        public AppData(DbContextOptions<AppData> options) : base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
